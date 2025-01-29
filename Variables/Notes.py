0fdf0e612c50a1bbd6cc5ff0f69f0ac83d927df9
sam1wammy <samantha.naranjo@ucas-edu.net>
2025-01-29 11:42:58 -0700

    python notes

--- Variables/Notes.py
+++ Variables/Notes.py
@@ -1,3 +1,12 @@
 # Samatha Naranjo, Variable notes
 
 print("it worked!")
+name ="Vienna"
+
+Subject =input("what is your favorite subject: ")
+
+small_number = 4
+
+name = "Tia"
+
+print(name,"Really likes", Subject)
