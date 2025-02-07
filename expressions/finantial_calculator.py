164e19e9ef1903208a9ec4134c4a5f431f3d1bf6
sam1wammy <samantha.naranjo@ucas-edu.net>
2025-02-07 11:47:45 -0700

    save in github

--- expressions/finantial_calculator.py
+++ expressions/finantial_calculator.py
@@ -1,5 +1,8 @@
 # samantha naranjo, finatial calculator
 
+from tkinter import Variable
+
+
 print("welcome user this program helps you calculate your savings ad how much you have to spend!you can only type the number!")
 
 income = float(input("what is your income?\n"))
@@ -10,29 +13,30 @@ utilities = float(input("what are your utilities?\n"))
 
 gorceries = float(input("what are your groceries?\n"))
 
-transportation = float(input("whhat transporation do you use?\n"))
-# calculate savings as 10% of income (income*.1)(variable)
+transportation = float(input("what transporation do you use?\n"))
+
 savings = income * 0.1
-# calculate spending as income-savings-rent-utilities-transportation-groceries (vaiable)
-spending = 
-# calculate percent income of rent (rent/income *100) (variable)
 
-# calculate percent income of utilities (utilities/income *100) (variable)
+spending = float(input("how much money do you spend\n"))
 
-# calculate percent income of groceries (groceries/income *100) (variable)
+income_rent = rent/income*100
 
-# calculate percent income of transportation (transportation/income *100) (variable)
+income_utitlities = utilities/income*100
 
-# calculate percent income of spending (spending/income *100) (variable)
+income_groceries = gorceries/income*100
 
-# your rent is $xx.xx which is xx% of your income. (print)
+income_transportation = transportation/income *100
 
-# your utilities is $xx.xx which is xx% of your income. (print)
+income_spending = spending/income *100
 
-# your groceries is $xx.xx which is xx% of your income. (print)
+your_rent = print ("Your rent is $rent which is income% of your income\n")
 
-# your transporation is $xx.xx which is xx% of your income. (print)
+your_utilitites = (print("your utilitites are $xx.xx which is xx% of your income\n "))
+ 
+your_groceries = (print("your groceries are $xx.xx which is xx% of your income\n"))
 
-# your savings is $xx.xx which is xx% of your income. (print)
+your_transportation = (print("your transportation is $xx.xx which is xx% of your income\n "))
 
-# your spending is $xx.xx which is xx% of your income. (print)
+your_savings = (print("your savings are $xx.xx which is xx% of your income\n"))
+
+your_spending = (print("your spending is $xx.xx which is xx% of your income\n"))
