d052ad21bd83b8ea1a3b96c5d074367021a9331d
sam1wammy <samantha.naranjo@ucas-edu.net>
2025-02-14 11:30:31 -0700

    silly sentence in py

---
+++ strings/silly_sentence.py
@@ -0,0 +1,9 @@
+#samantha naranjo
+
+word1 = input("enter your favorite icecream: ")
+word2 = input("enter a silly name: ")
+word3 = input("enter a silly action: ")
+
+story = f"{word2} is their name, they really like {word1} while they do {word3}!"
+
+print(story)
