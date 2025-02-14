4dffad6946085403367c6365ea1313d1eab955a0
sam1wammy <samantha.naranjo@ucas-edu.net>
2025-02-14 11:16:43 -0700

    decore name in c

---
+++ strings/name_decore.c
@@ -0,0 +1,16 @@
+//samantha naranjo, name decore
+#include <stdio.h>  
+  
+int main() {  
+    char name[50];  
+  
+    printf("What is your name? ");  
+    scanf("%s", name);  
+  
+    printf("\n");   
+    printf("<3   %s  <3\n", name);  
+   
+    
+  
+    return 0;  
+}  
