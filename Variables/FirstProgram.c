079e04e4da92bedd15c683524350749e3a96845d
sam1wammy <samantha.naranjo@ucas-edu.net>
2025-02-14 11:11:18 -0700

    name decore

--- Variables/FirstProgram.c
+++ Variables/FirstProgram.c
@@ -21,21 +21,4 @@ int main(void){
 
     char favorite_subject[] = "math";
     return 0;
-    }
-char name[] = "sami";
-
-int num_1 = 9;
- 
-int num_2 = 59;
-
-char breakfast[] = "I had a breakfast burrito!";
-
-char school_name[] = "UCAS";
-
-int year = 2025;
-
-char eye_color[] = "dark brown";
-
-int age = 14;
-
-char favorite_subject[] = "math";
+}
---
+++ strings/name_decore.py
@@ -0,0 +1,5 @@
+#samantha naranjo, name decore py
+
+name = input("what is your name? ")
+
+print(f"<3" + name + "<3")
