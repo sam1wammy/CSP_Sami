0c8e41b23a4c867619fe7c41cea1d01645ff28c6
sam1wammy <samantha.naranjo@ucas-edu.net>
2025-02-21 11:20:46 -0700

    idk bru

--- expressions/finantial_calculator.py
+++ expressions/finantial_calculator.py
@@ -1,8 +1,5 @@
 # samantha naranjo, finatial calculator
 
-from tkinter import Variable
-
-
 print("welcome user this program helps you calculate your savings ad how much you have to spend!you can only type the number!")
 
 income = float(input("what is your income?\n"))
@@ -11,7 +8,7 @@ rent = float(input("What is your rent\n"))
 
 utilities = float(input("what are your utilities?\n"))
 
-gorceries = float(input("what are your groceries?\n"))
+groceries = float(input("what are your groceries?\n"))
 
 transportation = float(input("how much is your transporation?\n"))
 
@@ -21,7 +18,7 @@ income_rent = rent/income*100
 
 income_utitlities = utilities/income*100
 
-income_groceries = gorceries/income*100
+income_groceries = groceries/income*100
 
 income_transportation = transportation/income *100
 
@@ -31,15 +28,10 @@ print(f"Your rent is ${rent:.2f} which is {income_rent:.2f}% of your income.")
 
 print(f"Your utilities are ${utilities:.2f} which is {income_utitlities:.2f}% of your income.")
 
-print(f"Your groceries are ${gorceries:.2f} which is {income_groceries:.2f}% of your income.")
+print(f"Your groceries are ${groceries:.2f} which is {income_groceries:.2f}% of your income.")
 
 print(f"Your transportation is ${transportation:.2f} which is {income_transportation:.2f}% of your income.")
 
 print(f"Your savings are ${savings:.2f} which is {savings_percentage:.2f}% of your income.")
 
-#your savings are $XX.XX which is XX% of your income. (Print)
-savings_percentage = (savings / income) * 100  # Calculate savings percentage
-print(f"Your savings are ${savings:.2f} which is {savings_percentage:.2f}% of your income.")
-
-#your spendings are $XX.XX which is XX% of your income. (Print)
-print(f"Your spendings are ${income:.2f} which is {savings_percentage:.2f}% of your income.")
+print(f"Your spendings are ${income-savings-transportation-income-groceries-utilities-rent:.2f} which is {income-savings-transportation-income-groceries-utilities-rent:.2f}% of your income.")
--- expressions/tempCodeRunnerFile.py
+++ expressions/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-utilities_percentage
+:
