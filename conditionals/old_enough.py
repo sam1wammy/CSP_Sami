6b7aee84dd716234dec3f2c5b09b6a3df40880e0
sam1wammy <samantha.naranjo@ucas-edu.net>
2025-02-26 11:17:12 -0700

    I did it!

---
+++ conditionals/old_enough.py
@@ -0,0 +1,14 @@
+#samantha naranjo, old enought assigment py
+
+age = int(input("how old are you?"))
+
+if age >= 18:  
+    print("You can vote.")  
+elif age >= 16:  
+    print("You can drive.")  
+elif age >= 15:  
+    print("You can get a learner's permit.")  
+elif age >= 5:  
+    print("You can go to school.")  
+else:  
+    print("You're not old enough for any of these.")  
