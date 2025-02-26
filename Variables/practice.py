a893d8b07fd80d3be430de623a42ae864ca58fb5
sam1wammy <samantha.naranjo@ucas-edu.net>
2025-02-26 11:26:12 -0700

    PY

--- Variables/practice.py
+++ Variables/practice.py
@@ -2,20 +2,40 @@
 
 name = "sami"
 
-samll_number = 8
+print("your name is:", name)
 
-big_number = 56
+small_num = 8
 
-breakfast = "I had a smootie for breakfast"
+print("your small number is:", small_num)
 
-favorite_color = "my favorite color is pink"
+big_num = 56
+
+print("your big number is:", big_num)
+
+breakfast = "smootie"
+
+print("for breakfast you had a:", breakfast)
+
+favorite_color = "pink"
+
+print("your favorite color is:", favorite_color)
 
 school = "ucas"
 
+print("your school is:", school)
+
 year = 2025
 
-eye_color = "my eyes are brown"
+print("the year is:", year)
 
-age = "im fourteen"
+eye_color = "brown"
 
-favorite_class = "I like lunch"
+print("your eyes are:", eye_color)
+
+age = "fourteen"
+
+print("your age is:", age)
+
+favorite_class = "math"
+
+print("your favorite class is:", favorite_class)
